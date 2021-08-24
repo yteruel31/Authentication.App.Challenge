20353838cf13e0e91605a1aab68765128f9635a6
Yoann TERUEL <yoann.teruel@gmail.com>
2021-08-24 18:00:33 +0200

    Protect Get & Patch User route with user id verification contains in token

---
+++ Authentication.App.Challenge.Web/AuthenticationHeader.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Authentication.App.Challenge.Web
+{
+    public class AuthenticationHeader
+    {
+        [FromHeader]
+        public string Authorization { get; set; }
+    }
+}
--- Authentication.App.Challenge.Web/Controllers/UserController.cs
+++ Authentication.App.Challenge.Web/Controllers/UserController.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using Authentication.App.Challenge.Repositories.Database.Dtos;
 using Authentication.App.Challenge.Services.Users;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
@@ -14,18 +15,27 @@ namespace Authentication.App.Challenge.Web.Controllers
     public class UserController : ControllerBase
     {
         private readonly IUserService _userService;
+        private readonly IAuthService _authService;
 
-        public UserController(IUserService userService)
+        public UserController(IUserService userService, IAuthService authService)
         {
             _userService = userService;
+            _authService = authService;
         }
 
         [HttpGet]
-        public async Task<IActionResult> Get(int id)
+        public async Task<IActionResult> Get(int id, [FromHeader] AuthenticationHeader header)
         {
             if (id == 0)
             {
                 return BadRequest();
+            }
+            
+            AuthService.Payload payload = _authService.Decode(header.Authorization[JwtBearerDefaults.AuthenticationScheme.Length..].Trim());
+
+            if (id != payload.Id)
+            {
+                return Unauthorized();
             }
             
             try
@@ -39,8 +49,15 @@ namespace Authentication.App.Challenge.Web.Controllers
         }
         
         [HttpPatch]
-        public async Task<IActionResult> Put(int id, [FromBody] JsonPatchDocument<UserDto> patchUser)
+        public async Task<IActionResult> Put(int id, [FromHeader] AuthenticationHeader header, [FromBody] JsonPatchDocument<UserDto> patchUser)
         {
+            AuthService.Payload payload = _authService.Decode(header.Authorization[JwtBearerDefaults.AuthenticationScheme.Length..].Trim());
+
+            if (id != payload.Id)
+            {
+                return Unauthorized();
+            }
+            
             try
             {
                 UserDto userToUpdate = await _userService.GetDto(id);
